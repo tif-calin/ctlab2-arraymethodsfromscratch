f4380391cbcff0dd65b89ab2046f5ff0ae32e475
culi <culitif@tuta.io>
2021-06-08 17:04:03 -0700

    add explanation of map function

--- src/ctlab2.js
+++ src/ctlab2.js
@@ -1,3 +1,32 @@
+/*
+
+const arr = [0, -5, 34, 3];
+
+arr.map(n => n ** 2);
+
+function map = ...
+
+const callback = n => n ** 2;
+map(arr, callback);
+
+function callback(n) {
+  return n + 5;
+}
+
+*/
+
+/*
+export function map(arr, callback) {
+  const newArr = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    const newElem = callback(elem);
+    newArr[i] = newElem;
+  }
+
+  return newArr;
+}
+*/
 
 export function map(arr, callback) {
   const newArr = [];
@@ -11,4 +40,8 @@ export function filter(arr, callback) {
   for(const elem of arr) if(callback(elem)) newArr[newArr.length] = elem;
 
   return newArr;
+}
+
+export function findIndex(arr, elem) {
+  return;
 }
