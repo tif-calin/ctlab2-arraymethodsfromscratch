199f3251b025f42bdf35585a07fb3d03bca485f0
culi <culitif@tuta.io>
2021-06-08 17:21:14 -0700

    skip holes in arrays

--- src/ctlab2.js
+++ src/ctlab2.js
@@ -13,14 +13,14 @@ export function map(arr, callback) {
 
 export function map(arr, callback) {
   const newArr = [];
-  for(let i = 0; i < arr.length; i++) newArr[i] = callback(arr[i]);
+  for(let i = 0; i < arr.length; i++) if(arr[i] !== undefined) newArr[i] = callback(arr[i]);
 
   return newArr;
 }
 
 export function filter(arr, callback) {
   const newArr = [];
-  for(const elem of arr) if(callback(elem)) newArr[newArr.length] = elem;
+  for(const elem of arr)  if(elem !== undefined && callback(elem)) newArr[newArr.length] = elem;
 
   return newArr;
 }
